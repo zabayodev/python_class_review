c27eb4dd386926a467d54a10b268bf19ffa42257
Emmanuel M <apple@Emmanuel-M.local>
2024-07-22 18:14:13 +0200

    Operator review

---
+++ addition.py
@@ -0,0 +1,4 @@
+number_1 = int(input("Enter the first number for addition: "))
+number_2 = int(input("Enter the second number for addition: "))
+sum_result = number_1 + number_2
+print(f"The sum of {number_1} and {number_2} is {sum_result}")
