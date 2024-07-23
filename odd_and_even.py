b0702a0151988f12814a8fbe87252959bf6a6ce7
Emmanuel M <apple@Emmanuel-M.local>
2024-07-23 12:35:12 +0200

    checking for odd and even in numbers

--- odd_and_even.py
+++ odd_and_even.py
@@ -1,5 +1,7 @@
 number = int(input("Enter a random number: "))
 if number % 2 == 0:
     print("The entered number is even")
+elif number == 0:
+    print("Please enter a valid number, division by zero is impossible")
 else:
     print("The entered number is odd")
